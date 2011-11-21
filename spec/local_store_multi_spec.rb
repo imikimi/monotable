776827957eece371d86aeaa55ffe96e1b0c49a26
Shane Brinkman-Davis <shanebdavis@imikimi.com>
2011-11-20 16:45:48 -0800

    added spec/local_store_multi_spec.rb

---
+++ spec/local_store_multi_spec.rb
@@ -0,0 +1,13 @@
+require File.join(File.dirname(__FILE__),"mono_table_helper_methods")
+require File.join(File.dirname(__FILE__),"common_api_tests")
+
+describe Monotable::LocalStore do
+  include MonotableHelperMethods
+
+  def blank_store
+    reset_temp_dir
+    Monotable::LocalStore.new(:store_paths=>[temp_dir],:initialize_new_multi_store=>true)
+  end
+
+  api_tests
+end
