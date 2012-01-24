c5e77f605a3476f95505b3ab3dd9dbcd1eec7e1c
shanebdavis <shanebdavis@gmail.com>
2012-01-24 11:41:18 -0800

    ServerClient api should now fully support async processign

--- lib/monotable/client/server_client.rb
+++ lib/monotable/client/server_client.rb
@@ -1,34 +1,11 @@
 # encoding: BINARY
+
+# https://github.com/archiloque/rest-client
+# http://rubydoc.info/gems/rest-client/1.6.7/frames
+# https://github.com/igrigorik/em-http-request/wiki/Issuing-Requests
 module Monotable
 
-  # Async calls return immediately.
-  # When the request complets later, either:
-  #   a) execute the passed in block on succcess with the response information passed in OR
-  #   b) execute the optional :on_error Proc. If none, the error is ignored (TODO: log the error)
-  module AsyncRestClientHelper
-    # options
-    #   :params => request params hash
-    #   :on_error => called on-error. Parameters: |http_request,error_message|
-    #   :accept_404 => on 404 results, parse the body and treat it as a success - the body should transfer the information that it was a 404
-    # block => called on success with the json-parsed response
-    def async_json_get(request_path,options={},&block)
-      headers = {:accept => :json}
-      http_request = EventMachine::HttpRequest.new(request_path).get :params => options[:params], :head => headers
-      on_error = options[:on_error] || Proc.new {|http_request|}
-
-      http_request.errback { on_error.call(http,"errback") }
-      http_request.callback do
-        if http_request.response_header.status == 200 || (options[:accept_404] && http_request.response_header.status == 404)
-          block.call(Tools.force_encoding(symbolize_keys(JSON.parse(http_request.response)),"ASCII-8BIT"))
-        else
-          # error
-          on_error.call(http,"invalid status code: #{http_request.response_header.status}")
-        end
-      end
-    end
-  end
-
-  module ReadClientHelper
+  module RestClientHelper
     private
 
     # return all values for "keys" in h as a hash
@@ -68,70 +45,77 @@ module Monotable
       fields = self[key]
       fields && MemoryRecord.new.init(key,fields)
     end
-  end
 
-  module RestClientHelper
+    def process_response(body,options)
+      if options[:raw_response]
+        body
+      else
+        Tools.force_encoding(
+          symbolize_keys(
+            JSON.parse(body),
+            options[:keys_to_symbolize_values]
+          ),
+          options[:force_encoding]
+        )
+      end
+    end
+
+    # Async calls return immediately.
+    # When the request complets later, either:
+    #   a) execute the passed in block on succcess with the response information passed in OR
+    #   b) execute the optional :on_error Proc. If none, the error is ignored (TODO: log the error)
+    # options
+    #   :body => string or Hash (payload)
+    #   :on_error => called on-error. Parameters: |http_request,error_message|
+    #   :keys_to_symbolize_values => {} passed into symbolize_keys
+    # block => called on success with the json-parsed response
+    def async_request(method,request_path,options={},&block)
+      headers = {:accept => :json}
+      on_error = options[:on_error] || Proc.new {|http_request,message| puts message;}
+
+      http_request = EventMachine::HttpRequest.new(request_path).send method, :body => options[:body], :params=> options[:params], :head => headers
+
+      http_request.errback { on_error.call(http_request,"request: #{method}|#{request_path}. errback called.") }
+      http_request.callback do
+        if http_request.response_header.status == 200 || (options[:accept_404] && http_request.response_header.status == 404)
+          block.call process_response(http_request.response,options)
+        else
+          # error
+          on_error.call(http_request,"request: #{method}|#{request_path}. invalid status code: #{http_request.response_header.status}")
+        end
+      end
+    end
+
     #request is the URI
     # options:
     #   :accept_404 => if true, treat 404 asif they were 200 messages (the assumption is the body encodes information about the 404 error)
     #   :params => request params
     #   :force_encoding => ruby string encoding type: Ex. "ASCII-8BIT"
-    def json_get(request_path,options={},&block)
+    # if you include a block, this uses async_reqeust
+    # for examples of using RestClient::Request.execute
+    #   https://github.com/archiloque/rest-client/blob/master/lib/restclient.rb
+    def request(method,request_path,options={},&block)
       request = "#{server}/#{request_path}"
-      return async_json_get request,options,&block if block
-      RestClient.get(request, :params=>options[:params], :accept=>:json) do |response, request, result|
+      return async_request method, request, options, &block if block
+      RestClient::Request.execute(
+        :method => method,
+        :url => request,
+        :payload => options[:body],
+        :headers => {
+          :params => options[:params],
+          :accept => :json,
+          :content_type => :json,
+        },
+        ) do |response, request, result|
         if response.code == 200 || (options[:accept_404] && response.code == 404)
-          return Tools.force_encoding(symbolize_keys(JSON.parse(response.body)),options[:force_encoding])
+          return process_response(response.body,options)
         end
-        raise NetworkError.new("invalid response code: #{response.code.inspect} for GET request: #{request.inspect}. Result: #{result.inspect}")
+        raise NetworkError.new("invalid response code: #{response.code.inspect} for #{method} request: #{request.inspect}. Result: #{result.inspect}")
       end
     end
 
-    # options
-    #   :payload can be a hash or a string
-    def put(request_path,options={},&block)
-      request = "#{server}/#{request_path}"
-      return async_put request,options,&block if block
-      RestClient.put(request, options[:payload]||{}) do |response, request, result|
-        return response.body if response.code == 200
-        raise NetworkError.new("invalid response code: #{response.code.inspect} for PUT request: #{request.inspect}. Result: #{result.inspect}")
-      end
-    end
-
-    # options
-    #   :payload can be a hash or a string
-    def post(request_path,options={},&block)
-      request = "#{server}/#{request_path}"
-      return async_post request,options,&block if block
-      RestClient.post(request, options[:payload]||{}) do |response, request, result|
-        return response.body if response.code == 200
-        raise NetworkError.new("invalid response code: #{response.code.inspect} for POST request: #{request.inspect}. Result: #{result.inspect}")
-      end
-    end
-
-    # options
-    #   :payload can be a hash or a string
-    def json_put(request_path,options={},&block)
-      request = "#{server}/#{request_path}"
-      return async_json_put request,options,&block if block
-      RestClient.put(request, options[:payload]||{}, :accept=>:json) do |response, request, result|
-        return symbolize_keys(JSON.parse(response.body)) if response.code == 200
-        raise NetworkError.new("invalid response code: #{response.code.inspect} for PUT request: #{request.inspect}. Result: #{result.inspect}")
-      end
-    end
-
-    # options
-    #   :payload can be a hash or a string
-    def json_post(request_path,options={},&block)
-      request = "#{server}/#{request_path}"
-      return async_json_post request,options,&block if block
-      RestClient.post(request, options[:payload]||{}, :accept=>:json) do |response, request, result|
-        return symbolize_keys(JSON.parse(response.body)) if response.code == 200
-        raise NetworkError.new("invalid response code: #{response.code.inspect} for POST request: #{request.inspect}. Result: #{result.inspect}")
-      end
-    end
-
-    def symbolize_keys(hash,keys_to_symbolize_values={})
+    def symbolize_keys(hash,keys_to_symbolize_values=nil)
+      keys_to_symbolize_values||={}
       ret={}
       hash.each do |k,v|
         k=k.to_sym
@@ -145,23 +129,22 @@ module Monotable
   # see ReadAPI
   module ServerClientReadAPI
     include ReadAPI
-    include ReadClientHelper
 
     # see ReadAPI
     def get(key,options={},&block)
-      json_get "records/#{key}", options.merge(:accept_404=>true, :force_encoding => "ASCII-8BIT"),&block
+      request(:get, "records/#{key}", :accept_404=>true, :force_encoding => "ASCII-8BIT", &block)
     end
 
     # see ReadAPI
-    def get_first(options={})
+    def get_first(options={},&block)
       request, params = prepare_get_first_request(options)
-      json_get request, options.merge(:params => params, :accept_404=>true, :force_encoding => "ASCII-8BIT")
+      request(:get, request, :params => params, :accept_404=>true, :force_encoding => "ASCII-8BIT", &block)
     end
 
     # see ReadAPI
-    def get_last(options={})
+    def get_last(options={},&block)
       request,params = prepare_get_last_request(options)
-      json_get request, options.merge(:params => params, :accept_404=>true, :force_encoding => "ASCII-8BIT")
+      request(:get, request, :params => params, :accept_404=>true, :force_encoding => "ASCII-8BIT", &block)
     end
   end
 
@@ -169,55 +152,44 @@ module Monotable
   module ServerClientWriteAPI
     include WriteAPI
 
-    KEYS_TO_SYMOBLIZE_VALUES={:result => true}
+    # this lists the KEYS of the returned JSON for which we want to automatically SYMBOLIZE their values.
+    KEYS_TO_SYMBOLIZE_VALUES={:result => true}
 
     # see WriteAPI
-    def set(key,fields)
-      request="#{server}/records/#{key}"
+    def set(key,fields,&block)
       fields = Tools.force_encoding(fields,"UTF-8")
-      RestClient.post(request, fields.to_json, :content_type => :json, :accept => :json) do |response, request, result|
-        return symbolize_keys(JSON.parse(response.body),KEYS_TO_SYMOBLIZE_VALUES) if response.code == 200
-        raise NetworkError.new("invalid response code: #{response.code.inspect} for GET request: #{request.inspect}. Result: #{result.inspect}")
-      end
+      request :post, "records/#{key}", :body => fields.to_json, :keys_to_symbolize_values => KEYS_TO_SYMBOLIZE_VALUES, &block
     end
 
     # see WriteAPI
-    def update(key,fields)
-      request="#{server}/records/#{key}"
+    def update(key,fields,&block)
       fields = Tools.force_encoding(fields,"UTF-8")
-      RestClient.put(request, fields.to_json, :content_type => :json, :accept => :json) do |response, request, result|
-        return symbolize_keys(JSON.parse(response.body),KEYS_TO_SYMOBLIZE_VALUES) if response.code == 200
-        raise NetworkError.new("invalid response code: #{response.code.inspect} for GET request: #{request.inspect}. Result: #{result.inspect}")
-      end
+      request :put, "records/#{key}", :body => fields.to_json, :keys_to_symbolize_values => KEYS_TO_SYMBOLIZE_VALUES, &block
     end
 
     # see WriteAPI
-    def delete(key)
-      request="#{server}/records/#{key}"
-      RestClient.delete(request, :accpet=>:json) do |response, request, result|
-        return symbolize_keys(JSON.parse(response.body),KEYS_TO_SYMOBLIZE_VALUES) if response.code == 200
-        raise NetworkError.new("invalid response code: #{response.code.inspect} for GET request: #{request.inspect}. Result: #{result.inspect}")
-      end
+    def delete(key,&block)
+      request :delete, "records/#{key}", :keys_to_symbolize_values => KEYS_TO_SYMBOLIZE_VALUES, &block
     end
   end
 
   # this api is supported for testing, it should never be used by an actual client
   module ServerClientServerReadAPI
-    def chunks; json_get("server/chunks")[:chunks]; end
-    def up?; json_get("server/heartbeat")[:status]=="alive"; end
-    def servers; json_get("server/servers")[:servers]; end
-    def chunk(id); json_get("server/chunk/#{id}"); end
-    def local_store_status; json_get("server/local_store_status"); end
+    def chunks; request(:get,"server/chunks")[:chunks]; end
+    def up?; request(:get,"server/heartbeat")[:status]=="alive"; end
+    def servers; request(:get,"server/servers")[:servers]; end
+    def chunk(id); request(:get,"server/chunk/#{id}"); end
+    def local_store_status; request(:get,"server/local_store_status"); end
   end
 
   # this api is supported for testing, it should never be used by an actual client
   module ServerClientServerModifyAPI
-    def balance; json_post("server/balance"); end
-    def join(server); json_put("server/join?server_name=#{server}"); end
+    def balance; request(:post,"server/balance"); end
+    def join(server); request(:put,"server/join?server_name=#{server}"); end
 
     # returns the raw chunk-file
-    def up_replicate_chunk(chunk_key); post("server/up_replicate_chunk/#{chunk_key}"); end
-    def down_replicate_chunk(chunk_key); post("server/down_replicate_chunk/#{chunk_key}"); end
+    def up_replicate_chunk(chunk_key); request(:post,"server/up_replicate_chunk/#{chunk_key}",:raw_response => true); end
+    def down_replicate_chunk(chunk_key); request(:post,"server/down_replicate_chunk/#{chunk_key}"); end
   end
 
   class ServerClient
@@ -226,9 +198,7 @@ module Monotable
     include ServerClientServerReadAPI
     include ServerClientServerModifyAPI
     include RestClientHelper
-    include AsyncRestClientHelper
     attr_accessor :server
-
 
     def initialize(server)
       @server=server
