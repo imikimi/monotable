989174f932547a218de3dfc1eeae7f2231a7cb5c
Shane Brinkman-Davis <shanebdavis@imikimi.com>
2012-02-22 10:33:13 -0800

    forgot to check in these files

---
+++ lib/monotable/local_store.rb
@@ -0,0 +1,42 @@
+require File.join(File.dirname(__FILE__),"xbd")
+require File.join(File.dirname(__FILE__),"version")
+
+require 'yaml'
+require 'cgi'
+require 'fileutils'
+require 'json'
+require 'rubygems'
+
+def monotable_require(relative_path,modules)
+  modules.each do |mod|
+    require File.join(File.dirname(__FILE__),relative_path.to_s,mod)
+  end
+end
+
+monotable_require :local_store, %w{
+  api
+  string
+  global
+  mini_event_machine
+  cache
+  record_cache
+  index_block_cache
+  constants
+  tools
+  file_handle
+  journal
+  journal_manager
+  compaction_manager
+  logger
+  column
+  columns
+  record
+  chunk
+  index_block
+  index_block_encoder
+  memory_chunk
+  disk_chunk_base
+  disk_chunk
+  path_store
+  local_store
+}
---
+++ spec/server_controller_2_spec.rb
@@ -0,0 +1,42 @@
+require File.expand_path(File.join(File.dirname(__FILE__),'..','lib','monotable','monotable'))
+require 'rubygems'
+require 'rest_client'
+require 'tmpdir'
+require 'fileutils'
+require 'net/http'
+require 'json'
+require 'uri'
+require File.expand_path(File.join(File.dirname(__FILE__),'daemon_test_helper'))
+require File.join(File.dirname(__FILE__),"mono_table_helper_methods")
+
+describe Monotable::HttpServer::ServerController do
+  include DaemonTestHelper
+
+  before(:all) do
+    start_daemon(:initialize_new_store=>true)
+  end
+
+  after(:all) do
+    shutdown_daemon
+  end
+
+  it "server/down_replicate_chunk should work" do
+
+    server_client.chunks.should == ["", "+++0", "++0", "+0", "0"]
+    server_client.chunk("0").should >= {"range_start"=>"0", "range_end"=>nil}
+
+    server_client.split_chunk("0","foo")
+
+    server_client.chunks.should == ["", "+++0", "++0", "+0", "0", "foo"]
+    server_client.chunk("0").should >= {"range_start"=>"0", "range_end"=>"foo"}
+    server_client.chunk("foo").should >= {"range_start"=>"foo", "range_end"=>nil}
+
+    server_client.down_replicate_chunk("foo")
+
+    server_client.chunks.should == ["", "+++0", "++0", "+0", "0"]
+    server_client.chunk("0").should >= {"range_start"=>"0", "range_end"=>"foo"}
+    server_client.chunk("foo").should == nil
+
+  end
+
+end
